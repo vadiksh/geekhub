f162026b7e1a8e2e3e09855e866e614d8aa5c293
Vadim <vadik.shevchenko@gmail.com>
2018-11-20 12:35:14 +0200

    updated

--- homework-2.2.js
+++ homework-2.2.js
@@ -1,51 +1,35 @@
+// max, min, sum, replaced min and max
+
+var arr = [NaN,undefined,-5,1,44,-12,-2,-1];
+homework(arr);
+
 function homework(array) {
-  var max;
-  var min;
   var minIndex;
   var maxIndex;
-  var sum = 0;
-  var result = {};
+  var result = {
+    sum: 0
+  };
 
   for (var i = 0; i < array.length; i++) {
-    if(array[i] == undefined || isNaN(array[i])) continue;
-    if(max == undefined || array[i] > max) max = array[i];
-  }
-  result['Max number'] = max;
+      if (array[i] && !isNaN(array[i])) {
+        result.sum = result.sum + array[i];
 
-  for (var i = 0; i < array.length; i++) {
-    if(array[i] == undefined || isNaN(array[i])) continue;
-    if (min == undefined || array[i] < min) min = array[i];
-  }
-  result['Min number'] = min;
-
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] == undefined || isNaN(array[i])) continue;
-    else sum = sum + array[i];
-  }
-  result['Sum'] = sum;
-
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] == undefined || isNaN(array[i])) continue;
-
-    if (maxIndex == undefined || array[i] > array[maxIndex]) {
-      maxIndex = i;
-      max = array[i];
+        if(!result.max || array[i] > result.max) {
+          result.max = array[i];
+          maxIndex = i;
+        } else if (!result.min || array[i] < result.min) {
+          result.min = array[i];
+          minIndex = i;
+        }
+      }
     }
+    array[minIndex] = result.max;
+    array[maxIndex] = result.min;
+    result.reordered = array;
 
-    if (minIndex == undefined || array[i] < array[minIndex]) {
-      minIndex = i;
-      min = array[i];
-    }  
-  }
-  array[minIndex] = max;
-  array[maxIndex] = min;
-  result['Reordered array'] = array;
-
-  return result;
+    return result;
 }
 
-var arr = [NaN,undefined,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-homework(arr);
 
 
 
@@ -60,3 +44,19 @@ function arrayToObject(array) {
 
 var arr = [7,3,-12,55,73];
 arrayToObject(arr);
+
+
+// function with callback
+
+function hideOddNumbers(from, to, callback) {
+  for (var i = from; i <= to; i++) {
+    if(!(i % 2)) {
+      console.log(i);
+    } 
+  }
+  callback();
+}
+function completed() {
+  console.log('function completed!');
+}
+hideOddNumbers(15, 25, completed);
